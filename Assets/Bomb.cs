d2821f72c212dd33b1db867c5a4c35be28ab780b
Haram Park <haram1117@khu.ac.kr>
2021-02-07 00:21:29 +0900

    20210207

---
+++ Assets/Bomb.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    public GameObject bomb;
+    public GameObject explosion;
+    public GameObject explosioninstance;
+    bool explosioncheck=false;
+
+    private void Update()
+    {
+        if (explosioncheck == true)
+        {
+            Invoke("Destroyexplosion", 2);
+        }
+        
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            Invoke("Destroybomb", 2);
+            explosioninstance=Instantiate(explosion, bomb.transform.position, Quaternion.identity);
+            explosioncheck = true;
+        }
+    }
+
+
+    void Destroybomb()
+    {
+        Destroy(bomb);
+    }
+
+    void Destroyexplosion()
+    {
+        Destroy(explosioninstance);
+    }
+}
